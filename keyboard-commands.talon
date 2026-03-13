165173684e91d3bd4b8a7be4aa1e4288dd8c047e
Markham Anderson <markham.anderson@meraki.net>
2026-03-13 07:29:29 -0700

    commands: toggle command vs dictation mode

--- keyboard-commands.talon
+++ keyboard-commands.talon
@@ -1,2 +1,4 @@
 key(pause): user.firm_toggle_talon()
+key(shift-pause): user.toggle_command_vs_dictation_mode()
 key(alt-t): speech.toggle()
+key(alt-c): user.toggle_command_vs_dictation_mode()
--- my_custom_actions.py
+++ my_custom_actions.py
@@ -6,37 +6,64 @@ mod = Module()
 MICROPHONE_DEVICE_LIST = ["None", "System Default"]
 
 def get_modes():
-    return scope.get("mode")
+  return scope.get("mode")
 
 def get_microphone_name():
-    return actions.sound.active_microphone() # => str ["None", "System Default"]
+  return actions.sound.active_microphone() # => str ["None", "System Default"]
 
 def is_microphone_none():
-    return get_microphone_name() == "None"
+  return get_microphone_name() == "None"
 
 def is_mode_sleep():
-    return "sleep" in get_modes()
+  return "sleep" in get_modes()
 
 def is_mode_dictation():
-    return "dictation" in get_modes()
+  return "dictation" in get_modes()
+
+def is_mode_command():
+  return "command" in get_modes()
 
 @mod.action_class
 class Actions:
-    def firm_toggle_talon():
-        """Toggle between None and System Default microphone"""
-        if is_microphone_none():
-            # Toggle on
-            subprocess.run(["mplayer", "/usr/share/sounds/freedesktop/stereo/message.oga"], check=True)
-            actions.sound.set_microphone("System Default")
-            actions.speech.enable()
-            actions.mode.disable("sleep")
-            actions.mode.disable("command")
-            actions.mode.enable("dictation")
-        else:
-            # Toggle off
-            subprocess.run(["mplayer", "/usr/share/sounds/freedesktop/stereo/power-unplug.oga"], check=True)
-            actions.sound.set_microphone("None")
-            actions.speech.disable()
-            actions.mode.disable("command")
-            actions.mode.disable("dictation")
-            actions.mode.enable("sleep")
+  def firm_toggle_talon():
+    """Toggle between not active and dictation mode"""
+    if is_microphone_none():
+      _set_dictation_mode()
+    else:
+      _set_sleep_mode()
+
+  def toggle_command_vs_dictation_mode():
+    """Toggle between command mode and dictation mode"""
+    if not is_mode_command():
+      _set_command_mode()
+    else:
+      _set_dictation_mode()
+
+def toggle_on():
+  actions.sound.set_microphone("System Default")
+  actions.speech.enable()
+
+def _set_command_mode():
+  subprocess.run(["mplayer", "/usr/share/sounds/freedesktop/stereo/message-new-instant.oga"], check=True)
+  if is_microphone_none():
+    toggle_on()
+  actions.mode.disable("sleep")
+  actions.mode.disable("dictation")
+  actions.mode.enable("command")
+
+def _set_dictation_mode():
+  subprocess.run(["mplayer", "/usr/share/sounds/freedesktop/stereo/message.oga"], check=True)
+  if is_microphone_none():
+    toggle_on()
+  actions.mode.disable("sleep")
+  actions.mode.disable("command")
+  actions.mode.enable("dictation")
+
+def _set_sleep_mode():
+  subprocess.run(["mplayer", "/usr/share/sounds/freedesktop/stereo/power-unplug.oga"], check=True)
+  if not is_microphone_none():
+    actions.sound.set_microphone("None")
+    actions.speech.disable()
+  actions.mode.disable("command")
+  actions.mode.disable("dictation")
+  actions.mode.enable("sleep")
